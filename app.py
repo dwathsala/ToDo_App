2a64ffe3dfa5cad26742bee931defe917851fa33
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-04-22 01:23:58 +0530

    Add GUI components for task display and entry in ToDo App

--- app.py
+++ app.py
@@ -7,4 +7,13 @@ root.geometry('400x500')
 root.resizable(False,False)
 root.iconbitmap('check.ico')
 
+heading = ttk.Label(root, text='All TASKS', font='arial 20 bold')
+heading.pack()
+
+frame = ttk.Frame(root, width=400, height=50)
+frame.pack(pady=2) 
+
+task_entry = ttk.Entry(frame, font='arial 18', width=28)
+task_entry.pack()
+
 root.mainloop()
