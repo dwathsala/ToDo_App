27fa296cda35742f3d55fee1e2ecdf5407220f2c
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-04-22 01:02:51 +0530

    Add initial ToDo App implementation with GUI using Tkinter

---
+++ app.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("ToDo App")
+root.geometry('400x500')
+root.resizable(False,False)
+root.iconbitmap('check.ico')
+
+root.mainloop()
