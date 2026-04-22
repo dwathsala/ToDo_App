e3cc6dc8a4ec1c15533302d817f116c811e3f74b
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-04-22 09:24:57 +0530

    Enhance GUI layout by adding listbox for task display and adjusting entry width

--- app.py
+++ app.py
@@ -7,13 +7,25 @@ root.geometry('400x500')
 root.resizable(False,False)
 root.iconbitmap('check.ico')
 
-heading = ttk.Label(root, text='All TASKS', font='arial 20 bold')
+heading = ttk.Label(root, text='All TASKS', font='arial 20 bold', foreground='darkblue')
 heading.pack()
 
 frame = ttk.Frame(root, width=400, height=50)
 frame.pack(pady=2) 
 
-task_entry = ttk.Entry(frame, font='arial 18', width=28)
+task_entry = ttk.Entry(frame, font='arial 18', width=27)
 task_entry.pack()
+
+frame1 = ttk.Frame(root, width=300, height=250)
+frame1.pack(pady=10)
+
+listbox = tk.Listbox(frame1, font='arial 12', width=39, height=18)
+listbox.pack()
+
+s = ttk.Style()
+s.configure('TButton', font='arial 12', foreground='red')
+
+delete_btn = ttk.Button(root, text='Delete Task', style='TButton')
+delete_btn.pack(pady=12,ipadx=10,ipady=12, side=tk.BOTTOM)
 
 root.mainloop()
